83b1cd600463f9b70a2c9fc60fe47bc712d0bdd2
RileyBagwell <riley@cnpbagwell.com>
2023-11-04 19:03:15 -0500

    Add Comments

--- src/LocationHandler.py
+++ src/LocationHandler.py
@@ -5,12 +5,15 @@ from dotenv import load_dotenv
 
 class LocationHandler:
     def __init__(self):
-        # Setup environmental variables
+        """Constructor function to set up environmental variables"""
         load_dotenv()
         self.mapquest_api_key = os.environ.get("MAPQUEST_API_KEY")
 
 
     def getDistance(self, address1, address2):
+        """Returns the time, in seconds, that it takes to travel from one address to another. Returns -1 if the
+        API request fails. Returns -2 if a different error occurs. Accepts two addresses, like the following example;
+        “750 Synergy Park Blvd, Richardson, TX”."""
         # Ensure all whitespace is removed from the strings
         address1 = address1.replace(' ', '+')
         address2 = address2.replace(' ', '+')
